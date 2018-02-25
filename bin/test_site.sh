8796216612cc85a11e53062c92f8393f96625ff7
Mark Reyes <mark.reyes@charter.com>
2018-02-25 11:08:16 -0600

    adding stuff

--- bin/test_site.sh
+++ bin/test_site.sh
@@ -3,5 +3,34 @@
 
 set -e # halt script on error
 
-bundle exec jekyll build
-bundle exec htmlproofer ./_site --only-4xx --check-favicon --check-html --disable-external
+function header() {
+  echo "---------------"
+}
+
+function build_site() {
+    echo "bundle exec jekyll build"
+    bundle exec jekyll build
+}
+
+function test_site() {
+    echo "bundle exec htmlproofer ./_site"
+    bundle exec htmlproofer ./_site \
+    --only-4xx \
+    --check-html \
+    --disable-external \
+    --empty-alt-ignore
+}
+
+
+
+#Main
+echo "Executing Jekyll test script"
+
+header
+echo -e "Building site\n"
+build_site
+echo -e "\n\n"
+
+header
+echo -e "Testing _site\n"
+test_site
